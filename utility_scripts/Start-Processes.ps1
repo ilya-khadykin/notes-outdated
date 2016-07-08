583ea42ae215632f7078e7615f86f1f2e8ede32a
Ilya Khadykin <mageofit@gmail.com>
2016-07-08 19:12:25 +0300

    Strart-Processes.ps1 powershell script`

---
+++ utility_scripts/Start-Processes.ps1
@@ -0,0 +1,14 @@
+﻿# configuration
+$code_path="C:\Windows\System32\cmd.exe"
+
+$file_path="C:\Windows\System32\cmd.exe"
+$arguments="/K cd $code_path & dir"
+
+$i=1
+DO
+{
+  "Starting process $i"
+  $process
+  $i++
+  Start-Process -FilePath $file_path -ArgumentList $arguments
+} While ($i -le 1)
